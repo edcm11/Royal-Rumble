7db0d3cdc7fac8a7e48e8c73a0cf21dc60ea26b3
Eliot Cano <eliot7cano@gmail.com>
2018-09-04 08:01:44 -0500

    ARchivos de inicio

---
+++ main.js
@@ -0,0 +1,27 @@
+//canvas config
+var canvas = document.getElementsByTagName('canvas')[0];
+var ctx = canvas.getContext('2d')
+
+//testing
+//ctx.fillRect(0,0,50,50)
+
+//Variables globales
+
+
+//clases
+class Board{
+  this.x = 0
+  this.y = 0
+}
+
+//instancias
+
+
+//funciones principales
+
+
+//funciones auxiliares
+
+
+//los observadores
+ 
